f560df76ead0c05255bd163b9a155b68fe50bd18
MrtnOmwenga <martin36449@gmail.com>
2022-09-11 10:42:42 -0700

    Variables, if else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* your code goes there */
+int c;
+ int d;
+for (c = 0; c < 10; c++)
+{
+for (d = c+1; d < 10 ; d++)
+{
+putchar((c % 10) + '0');
+putchar((d % 10) + '0');
+if (c < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
