985a18ab5e3f5025d45d8b4a49e40ae325587399
MrtnOmwenga <martin36449@gmail.com>
2022-09-12 02:51:04 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,8 @@
 int main(void)
 {
 int c;
-char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+;
 for (c = 0 ; c < 59 ; c++)
 {
 putchar(string[c]);
