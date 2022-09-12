f3dce33a0998d2b0b6a531af471c8a527b8c193c
MrtnOmwenga <martin36449@gmail.com>
2022-09-12 03:17:16 -0700

    Variables, if else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,15 +17,29 @@ for (c = 0; c < 100; c++)
 {
 for ( e = c + 1 ; e < 100 ; e++)
 {
+if (c > 9)
+{
 d = c;
 while (d > 10)
 {
 d = d / 10;
 }
+}
+else
+{
+d = 0;
+}
+if (e > 9)
+{
 f = e;
 while (f > 10)
 {
 f = f / 10;
+}
+}
+else
+{
+f = 0;
 }
 putchar((d % 10) + '0');
 putchar((c % 10) + '0');
