7139f5cd4de6b4f792b07d8c8cc2a3c0e56763cb
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 06:13:55 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int c)
 {
 if (c > 0)
 {
-return (1);
 putchar('+');
+return (1);
 }
 else if (c == 0)
 {
-return (0);
 putchar(0 + '0');
+return (0);
 }
 else
 {
-return(-1);
 putchar('-');
+return (-1);
 }
 }
