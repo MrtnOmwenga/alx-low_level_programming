87363c5eb4970df52622f75b8cd0e8dd975b64fe
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 02:38:57 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * _islower - Prints a character
+ *
+ * @c: character to be checked
+ *
+ * Return: void
+ *
+ */
+void _islower(int c)
+{
+if ( islower(c) )
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
