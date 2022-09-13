ca1b3ed54addb4f5e13b12dc86c4a81633549b6b
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 01:46:33 -0700

    Funcyions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* your code goes there */
+char string[] = "_putchar\n";
+int c;
+for (c = 0; c < 9; c++)
+{
+putchar(string[c]);
+}
+return (0);
+}
