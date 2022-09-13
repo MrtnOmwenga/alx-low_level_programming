e1b8bfb29e59c4b88fda4f195cba2494d1bea404
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 08:04:47 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,21 @@ int c;
 int d;
 int e;
 int f = 0;
+int g;
 for (c = 0; c < 10; c++)
 {
 for (d = 0; d < 10; d++)
 {
 e = d * f;
-putchar(e + '0');
+if (e > 9)
+{
+g = e
+while (g > 9)
+{
+g = g / 10;
+}
+putchar(g + '0');
+putchar((e % 10) + '0');
 putchar(',');
 putchar(' ');
 }
