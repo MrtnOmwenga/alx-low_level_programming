e1322da2812cfa041345ccebf98fc5b2c6f888cd
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 07:33:19 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *jack_bauer - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+/* your code goes there */
+int c;
+int d;
+int f;
+int e;
+for (c = 0; c < 24; c++)
+{
+for (e = c + 1 ; e < 60 ; e++)
+{
+if (c > 9)
+{
+d = c;
+while (d >= 10)
+{
+d = d / 10; }}
+else
+{
+d = 0; }
+if (e > 9)
+{
+f = e;
+while (f >= 10)
+{
+f = f / 10; }}
+else
+{
+f = 0; }
+putchar((d % 10) + '0');
+putchar((c % 10) + '0');
+putchar(':');
+putchar((f % 10) + '0');
+putchar((e % 10) + '0');
+putchar(' ');
+}}}}
