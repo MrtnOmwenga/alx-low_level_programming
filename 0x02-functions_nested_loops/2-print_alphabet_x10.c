276cb6f106e4ff85a022fe5261375ef52d9b1366
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 02:28:44 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * print_alphabet_x10 - Prints a character
+ *
+ * Return: void
+ *
+ */
+void print_alphabet_x10(void)
+{
+int a;
+char c;
+for (a = 0; a < 10; a++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
