d9adf08319048ba4f9bdca99b8d0e7dc087c34b1
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 06:34:52 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * _abs - Prints a character
+ *
+ * @c: character to be checked
+ *
+ * Return: int
+ *
+ */
+int _abs(int c)
+{
+c = abs(c);
+return c;
+}
