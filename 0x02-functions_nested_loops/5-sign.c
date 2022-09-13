72cd71abe8d2199e73b138be90d351b906711904
MrtnOmwenga <martin36449@gmail.com>
2022-09-13 06:05:35 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * print_sign - Prints a character
+ *
+ * @c: character to be checked
+ *
+ * Return: int
+ *
+ */
+int print_sign(int c)
+{
+if (c > 0)
+{
+putchar('+');
+return (1);
+}
+else if (c = 0)
+{
+putchar(0 + '0');
+return (0);
+}
+else
+{
+print('-');
+return(-1);
+}
+}
