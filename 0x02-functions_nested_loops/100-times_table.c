21a5648467ca339b731a9d99d8011d6a7dc67b8a
MrtnOmwenga <martin36449@gmail.com>
2022-09-14 02:41:04 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,6 +5,8 @@
 /**
  *print_times_table - Prints a times table
  *
+ * @n: Number for table
+ *
  * Return: void
  *
  */
@@ -14,7 +16,6 @@ int c;
 int d;
 int e;
 int f = 0;
-
 if (n <= 15 && n >= 0)
 {
 for (c = 0; c <= n; c++)
@@ -24,7 +25,9 @@ for (d = 0; d <= n; d++)
 e = d * f;
 if (e > 9 && e < 100)
 {
-putchar(' ');
+if (d > 0)
+{
+putchar(' '); }
 printf("%d", e);
 if (d < n)
 {
@@ -32,8 +35,10 @@ putchar(',');
 putchar(' '); }}
 else if (e <= 9)
 {
+if (d > 0)
+{
 putchar(' ');
-putchar(' ');
+putchar(' '); }
 putchar(e + '0');
 if (d < n)
 {
