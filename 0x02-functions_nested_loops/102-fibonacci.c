11038364c85aa27ec89f30b0d6115e29cf15f97a
MrtnOmwenga <martin36449@gmail.com>
2022-09-14 06:28:16 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints a times table
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int c, d = 1, e = 2, f;
+printf("%d, ",d);
+printf("%d, ", e);
+for (c = 0; c < 48; c++)
+{
+f = e + d;
+printf("%d, ", f);
+d = e;
+e = f;
+}
+}
