a4e73c09952f6847a3e6930bc72ed240d6b6dcd6
MrtnOmwenga <martin36449@gmail.com>
2022-09-15 00:55:06 -0700

    Functions nested loops"

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper: Checks whether character passed is even
+ *
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+if (isupper(c) == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
