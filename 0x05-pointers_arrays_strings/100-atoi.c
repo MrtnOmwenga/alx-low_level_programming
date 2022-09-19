c712aacb3349ca0ed7eaeaa87a08787474532f8d
MrtnOmwenga <martin36449@gmail.com>
2022-09-19 11:36:45 -0700

    Pointers, arrays, strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *_atoi- Converts string to number
+ *
+ *@s: String to be converted
+ *
+ *Return: int
+ */
+
+int _atoi(char *s)
+{
+int c = atoi(c);
+return(c);
+}
