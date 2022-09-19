e43e31798e2787fb506056e979095a5d5a7c05cf
MrtnOmwenga <martin36449@gmail.com>
2022-09-19 10:54:14 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_stprcpy- Copies one string to another
+ *
+ *@dest: String to be copied to
+ *@src: String to be copied from
+ *
+ *Return: Destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+
+return (dest);
+}
