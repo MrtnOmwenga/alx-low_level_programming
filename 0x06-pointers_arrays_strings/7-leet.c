3796e53012335b6600533b74d5786d1a498ff719
MrtnOmwenga <martin36449@gmail.com>
2022-09-21 15:30:11 -0700

    Pointers arrays string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *leet- Converts string to 1337
+ *
+ *@str: String to be converted
+ *
+ *Return: char *
+ */
+
+char *leet(char *str)
+{
+int c, d;
+char chars[11] = "aAeEoOtTlL";
+char leet[11] = "4433007711";
+while (str[c])
+{
+for (d = 0; d < 10; d++)
+{
+if (str[c] == chars[d])
+{
+str[c] = leet[d];
+}
+}
+c++;
+}
+return (str);
+}
