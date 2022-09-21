d107fc64dfd7fba7d7be429c30c69ebada74ca36
MrtnOmwenga <martin36449@gmail.com>
2022-09-21 15:06:45 -0700

    Pointers arrays string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *cap_string- Capitalizes the first letter of each word
+ *
+ *@str: String to be capitalized
+ *
+ *Return: char *
+ */
+
+char *cap_string(char *str)
+{
+int c = 0, d;
+char del[14] = " \t\n,;.!?\"(){}";
+while (str[c])
+{
+for (d = 0; d < 14; d++)
+{
+if (str[c] == del[d])
+{
+str[c + 1] = toupper(str[c + 1]);
+}
+}
+c++;
+}
+return (str);
+}
