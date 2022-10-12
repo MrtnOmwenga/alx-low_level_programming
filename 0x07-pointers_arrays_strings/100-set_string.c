3ef0d1a5ebdfb17c9a733e4e421f2b3c328bebd0
MrtnOmwenga <martin36449@gmail.com>
2022-10-12 09:33:20 -0700

    Pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *set_string- Set value of pointer to string
+ *
+ *@s: Pointer
+ *@to: String
+ *
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ int i, j;
 int diag1 = 0, diag2 = 0;
 for (i = 0; i < size; i++)
 {
-diag1 += a[i][i];
+diag1 = diag1 + a[i][i];
 }
 for (j = size; j > 0; j--)
 {
