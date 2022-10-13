5e2c886f3248f34a327e7cfb04313db8124999ff
MrtnOmwenga <martin36449@gmail.com>
2022-10-13 08:37:36 -0700

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *recursion2- Recursion
+ *
+ *@n: Number
+ *@j: Counter
+ *
+ *Return: int
+ */
+
+int recursion2(int n, int j)
+{
+if (j < n)
+{
+if (n % j == 0)
+{
+return (0);
+}
+else
+{
+return (recursion2(n, j + 1));
+}
+}
+else
+{
+return (1);
+}
+}
+
+/**
+ *is_prime_number- Checks whether a number is a prime number
+ *
+ *@n: Number to be checked
+ *
+ *Return: Int
+ */
+
+int is_prime_number(int n)
+{
+if (n < 0)
+{
+n *= -1;
+}
+if (n == 1)
+{
+return (0);
+}
+else
+{
+return (recursion2(n, 2));
+}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+int is_prime_number(int n);
+int recursion2(int n, int j);
 int recursion(int n, int j);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
