f48758c7d9d4f1c21856839272a8fbd4433c1a5d
MrtnOmwenga <martin36449@gmail.com>
2022-10-13 08:51:02 -0700

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,17 +18,13 @@ if (j < n / 2)
 int next = j + 1;
 if (j * j == n)
 {
-return (j);
-}
+return (j); }
 else
 {
-return (recursion(n, next));
-}}
+return (recursion(n, next)); }}
 else
 {
-return (-1);
-}
-}
+return (-1); }}
 
 /**
  *_sqrt_recursion- Finds the square root of a number
@@ -44,18 +40,13 @@ int e;
 if (n > 1)
 {
 e = recursion(n, 1);
-return (e);
-}
+return (e); }
 else if (n == 1)
 {
-return (1);
-}
+return (1); }
 else if (n == 0)
 {
-return (0);
-}
+return (0); }
 else
 {
-return (-1);
-}
-}
+return (-1); }}
