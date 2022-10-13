d389d6ac25049c711a83b35f348b398df2a563bc
MrtnOmwenga <martin36449@gmail.com>
2022-10-13 10:20:33 -0700

    Argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- Prints the product of two numbers
+ *
+ *@argc: Argument count
+ *@argv: Numbers
+ *
+ *Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+int e = atoi(argv[1]);
+int f = atoi(argv[2]);
+printf("%d\n", e * f);
+return (0);
+}
+}
