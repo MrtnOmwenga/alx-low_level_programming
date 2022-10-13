20d39f0eb10c60b8c57c2b27d7c3049b96214add
MrtnOmwenga <martin36449@gmail.com>
2022-10-13 07:02:00 -0700

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  *_pow_recursion- Returns a number raised to the power of another
  *
  *@x: Base
- #@y: Index
+ *@y: Index
  *
  *Return: int
  */
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 {
 if (y > 0)
 {
-return (x * _pow_recursion(x, y-1));
+return (x * _pow_recursion(x, y - 1));
 }
 else if (y == 0)
 {
