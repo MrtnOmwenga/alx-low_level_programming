11d41a34f6af15d10331e0048ab5049d9a595916
MrtnOmwenga <martin36449@gmail.com>
2022-10-25 08:29:00 -0700

    Format change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog- Dog details
+ *
+ *@name: Dog name
+ *@age: Dog's age
+ *@owner: Dog's owner
+ *
+ *Description: Contains dog's details
+ */
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
