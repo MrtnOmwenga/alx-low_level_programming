cc493e77eb6f0cc11a7c31bcb13bf5e053f1e065
MrtnOmwenga <martin36449@gmail.com>
2022-10-26 08:36:33 -0700

    Debugging

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *my_dog = malloc(sizeof(dog_t));
 
-char *dog_name = name;
-float dog_age = age;
-char *dog_owner = owner;
-
 if (my_dog == NULL)
 {
-free(my_dog.name);
-free(my_dog.age);
-free(mydog.owner);
+free(my_dog->name);
+free(my_dog->age);
+free(mydog->owner);
 free(my_dog);
 return (NULL);
 }
