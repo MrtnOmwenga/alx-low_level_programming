80c596a2e0fcd0576e1cee06c211fafa9eb05570
MrtnOmwenga <martin36449@gmail.com>
2022-11-21 09:35:42 -0800

    debugging

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,11 +15,8 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *ptrNew;
-if (ptr == NULL)
-{
-return (NULL);
-}
-else if (new_size == 0)
+
+if (new_size == 0)
 {
 free(ptr);
 return (NULL);
