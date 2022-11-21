26f67f4399befef92c61df9056d0d10f9a153133
MrtnOmwenga <martin36449@gmail.com>
2022-11-21 07:31:41 -0800

    Array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,30 +11,28 @@
 
 int *array_range(int min, int max)
 {
-  int *numbers;
-  int i = 0;
+int *numbers;
+int i = 0;
 
-  if (max < min)
-    {
-      return (NULL);
-    }
-  else
-    {
-      numbers = (int *)malloc((max - min + 1) * sizeof(int));
-      if (numbers == NULL)
-	{
-	  free (numbers);
-	  return (NULL);
-	}
-      else
-	{
-	  while (min <= max)
-	    {
-	      numbers[i] = min;
-	      min++;
-	      i++;
-	    }
-	  return (numbers);
-	}
-    }
+if (max < min)
+{
+return (NULL);
 }
+else
+{
+numbers = (int *)malloc((max - min + 1) * sizeof(int));
+if (numbers == NULL)
+{
+free (numbers);
+return (NULL);
+}
+else
+{
+while (min <= max)
+{
+numbers[i] = min;
+min++;
+i++;
+}
+return (numbers);
+}}}
