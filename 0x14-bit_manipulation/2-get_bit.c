7ab25bb6bcfe97d052b4906932acb41b540dc93b
MrtnOmwenga <martin36449@gmail.com>
2022-11-25 15:35:57 -0800

    Get bit function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+
+/**
+ *get_bit- Gets bit of a certain index
+ *
+ *
+ *@n: Integer
+ *@index: Index
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i = 0;
+if (n == 0)
+{
+return (0);
+}
+ 
+while (n > 0)
+{
+if (i == index)
+{
+return (n % 2);
+}
+i++;
+n /= 2;
+}
+return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef BITWISE_OPERATORS
 #define BITWISE_OPERATORS
 
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
