0c9db5de72e20053a0fb30aa8507ae00a51ec2a8
MrtnOmwenga <martin36449@gmail.com>
2022-11-26 04:53:31 -0800

    Clear bit function

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *clear_bit- Sets bit to 1
+ *
+ *@n: Integer to be changes
+ *@index: Index
+ *
+ *Return: int
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int decimal_num = 0, binary_num = 1, base = 1;
+unsigned int i;
+
+if (*n == 0 && index != 0)
+{
+return (-1);
+}
+if (index > 500)
+{
+return (-1);
+}
+for (i = 0; i < index; i++)
+{
+binary_num *= 10;
+}
+
+while (binary_num > 0)
+{
+decimal_num += (binary_num % 10) * base;
+binary_num /= 10;
+base *= 2;
+}
+*n -= decimal_num;
+return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef BITWISE_OPERATORS
 #define BITWISE_OPERATORS
 
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
