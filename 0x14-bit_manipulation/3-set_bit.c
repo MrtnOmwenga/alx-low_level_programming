979e982651477f75c7821460783c11c3b2cd2f78
MrtnOmwenga <martin36449@gmail.com>
2022-11-26 04:25:42 -0800

    Handling out of range index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  *set_bit- Sets bit to 1
  *
@@ -12,6 +14,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 unsigned long int decimal_num = 0, binary_num = 1, base = 1;
 unsigned int i;
 
+if (index > 500)
+{
+return (-1);
+}
 for (i = 0; i < index; i++)
 {
 binary_num *= 10;
@@ -23,13 +29,6 @@ decimal_num += (binary_num % 10) * base;
 binary_num /= 10;
 base *= 2;
 }
-if (decimal_num > *n)
-{
 *n += decimal_num;
 return (1);
-}
-else
-{
-return (-1);
-}
 }
