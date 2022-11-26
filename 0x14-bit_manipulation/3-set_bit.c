63ab2a2a64661891daaab2197583d60b163cf51d
MrtnOmwenga <martin36449@gmail.com>
2022-11-26 02:26:45 -0800

    Set bit function

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,29 @@
+/**
+ *set_bit- Sets bit to 1
+ *
+ *@n: Integer to be changes
+ *@index: Index
+ *
+ *Return: int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int decimal_num = 0, binary_num = 1, base = 1;
+unsigned int i;
+
+for (i = 0; i < index; i++)
+{
+binary_num *= 10;
+}
+
+while (binary_num > 0)
+{
+decimal_num += (binary_num % 10) * base;
+binary_num /= 10;
+base *= 2;
+}
+  
+*n += decimal_num;
+return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef BITWISE_OPERATORS
 #define BITWISE_OPERATORS
 
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
