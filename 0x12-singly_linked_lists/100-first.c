a773c05e5a58407cd0458983a096c9f305cd940b
MrtnOmwenga <martin36449@gmail.com>
2022-11-27 11:51:38 -0800

    Constructor function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *my_funct- Prints before main
+ *
+ *Return: void
+ */
+
+void my_funct(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
