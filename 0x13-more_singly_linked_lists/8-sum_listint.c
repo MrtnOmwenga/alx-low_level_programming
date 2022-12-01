14fd97972501d6617269ed8a80ae977f8b04077a
MrtnOmwenga <martin36449@gmail.com>
2022-12-01 03:49:49 -0800

    Sum list int

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ *sum_listint- Prints sum of elements in list
+ *
+ *@head: Linked list
+ *
+ *Return: int
+ */
+
+int sum_listint(listint_t *head)
+{
+listint_t *n;
+int sum = 0;
+if (head == NULL)
+{
+return (0);
+}
+n = head;
+sum += n->n;
+while (n != NULL)
+{
+sum += n->n;
+n = (*n).next;
+}
+return (sum);
+}
