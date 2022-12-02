5812f8cf6ecec7dc2495c8e4f36911e43eb0d995
MrtnOmwenga <martin36449@gmail.com>
2022-12-01 21:06:00 -0800

    Diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ int diag1 = 0, diag2 = 0;
 for (i = 0; i < size; i++)
 {
 diag1 = diag1 + a[(size * i) + i];
-diag2 = diag2 + a[(size * (i + 1)) - (i + 1)]; 
+diag2 = diag2 + a[(size * (i + 1)) - (i + 1)];
 }
-printf("%d, %d", diag1, diag2);
+printf("%d, %d\n", diag1, diag2);
 }
