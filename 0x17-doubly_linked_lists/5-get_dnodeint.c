4d421525a334776705ad72232571e61d66db5d73
MrtnOmwenga <martin36449@gmail.com>
2022-12-08 06:34:17 -0800

    Restructured code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,19 +14,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 unsigned int i = 0;
 dlistint_t *n;
 
-if (head == NULL)
-{
-return (NULL);
-}
 n = head;
-while (n->next != NULL)
+if (index == 0)
 {
+return (n);
+}
+while (n != NULL)
+{
+n = n->next;
+i++;
 if (i == index)
 {
 return (n);
 }
-i++;
-n = n->next;
 }
 return (NULL);
 }
