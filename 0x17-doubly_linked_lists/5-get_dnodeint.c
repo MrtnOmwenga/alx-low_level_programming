59e991e3f43df1b5122072e05de1cc4f20a998a7
MrtnOmwenga <martin36449@gmail.com>
2022-12-08 06:05:45 -0800

    Handling null values

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,6 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 unsigned int i = 0;
 dlistint_t *n;
 
+if (*head == NULL || head == NULL)
+{
+return (NULL)
+}
 n = head;
 while (n->next != NULL)
 {
