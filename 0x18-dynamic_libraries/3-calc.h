1f5f7a489ef9e043e692ec7d51daf285451c11fb
MrtnOmwenga <martin36449@gmail.com>
2022-12-13 10:53:45 -0800

    Debugging

--- 0x18-dynamic_libraries/3-calc.h
+++ 0x18-dynamic_libraries/3-calc.h
@@ -2,8 +2,6 @@
 #define CALC_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * struct op - Struct op
@@ -17,11 +15,11 @@ typedef struct op
   int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
