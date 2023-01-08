f63703ec6654abbb5e0bb63996c06cabcb1aa297
MrtnOmwenga <martin36449@gmail.com>
2023-01-08 11:06:47 -0800

    Debugging

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,8 +13,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 unsigned long int hash, index;
 
-hash = hash_dbj2(key);
-index = has % size;
+hash = hash_djb2(key);
+index = hash % size;
 
 return (index);
 }
