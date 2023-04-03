72a06244349098e68493f28ccefbf8225078a465
MrtnOmwenga <martin36449@gmail.com>
2023-04-02 23:57:19 -0700

    Debugging

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,7 @@ int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[])  
