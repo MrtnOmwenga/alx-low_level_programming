90446f9bcae213a08aeb277b82336dff7e346c9a
MrtnOmwenga <martin36449@gmail.com>
2023-04-13 10:52:01 -0700

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ *linear_search- Linear search algorithm
+ *
+ *@array: Array to be searched
+ *@size: Size of array
+ *@value: Value being searched for
+ *
+ *Return: int
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+  size_t count = 0;
+  if (array == NULL)
+    {
+      return (-1);
+    }
+
+  while (count < size)
+    {
+      printf("Value checked array[%ld] = [%d]\n", count, array[count]);
+      if (array[count] == value)
+	{
+	  return (count);
+	}
+      count++;
+    }
+  return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/** Libraries **/
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Functions **/
+int linear_search(int *array, size_t size, int value);
+
+/**Utilities**/
+
+#endif
